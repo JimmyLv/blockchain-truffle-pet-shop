8924caa866d93b9049945c689c85f330effb7758
Jimmy Lv <jimmy.jinglv@gmail.com>
2018-02-04 18:35:45 +0800

    Finished the compile, deploy(migration) and Testing

---
+++ test/TestAdoption.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.17;
+
+import 'truffle/Assert.sol';
+import 'truffle/DeployedAddresses.sol';
+import '../contracts/Adoption.sol';
+
+contract TestAdoption {
+    Adoption adoption = Adoption(DeployedAddresses.Adoption());
+ 
+    function testUserCanAdoptPet() public {
+        // given
+
+        // when
+        uint returnedId = adoption.adopt(8);
+
+        // then
+        Assert.equal(returnedId, 8, 'Adoption of pet Id 8 should be recorded.');
+    }
+
+    function testGetAdopterAddressByPetId() public {
+        address expected = this;
+
+        address adopter = adoption.adopters(8);
+
+        Assert.equal(adopter, expected, 'Owner of pet Id 8 should be recorded.');
+    }
+
+    function testGetAdopterAddressByPetIdInArray() public {
+        address expected = this;
+
+        address[16] memory adopters = adoption.getAdopters();
+
+        Assert.equal(adopters[8], expected, 'Owner of pet Id 8 should be recorded.');
+    }
+}
